74ab1e775607e6a0c425b13cafa36700ce17d541
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-08-27 07:27:52 +0600

    call button functionality added

---
+++ script/script.js
@@ -0,0 +1,46 @@
+function getId(id) {
+    return document.getElementById(id);
+}
+
+const cardBox = getId('card-box');
+const callBtn = document.getElementsByClassName('call-btn')
+const coin = getId('coin')
+
+
+
+
+// call button functionality
+cardBox.addEventListener('click', function (e) {
+    if (e.target.className.includes('call-btn')) {
+        const callBtn = e.target
+        const cardTitle = callBtn.parentNode.parentNode.children[1].innerText
+        const cardSubTitle = callBtn.parentNode.parentNode.children[2].innerText
+        const cardNumber = callBtn.parentNode.parentNode.children[3].innerText
+        const localTime = new Date().toLocaleTimeString()
+        const coinNumber = Number(coin.innerText)
+        const callCharge = 20
+        const currentCoinNum = coinNumber - callCharge;
+        if (currentCoinNum < 0) {
+            alert('no  balnce')
+            return;
+        }
+        coin.innerText = currentCoinNum
+        alert('📞 Calling  ' + cardSubTitle + ' '+ cardNumber + '...')
+
+        const historyBox = getId('history')
+
+        const newHistory = document.createElement('div')
+        newHistory.innerHTML = `
+            <div class="flex justify-between items-center bg-[#5c5c5c09] p-3 rounded-lg mt-4">
+                            <div>
+                                <h1 class="font-bold">${cardTitle}</h1>
+                                <p>${cardNumber}</p>
+                            </div>
+                            <p>${localTime}</p>
+                        </div>
+        `
+
+        historyBox.append(newHistory)
+
+    }
+})
